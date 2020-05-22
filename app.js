ddf7a79c8754ff5fd229fe2d48f264c7e40b6b8b
Jackson Cai <jcai8649@gmail.com>
2020-05-21 23:56:06 -0700

    Adjust book input format

--- app.js
+++ app.js
@@ -8,18 +8,8 @@ function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    read ? this.read ="read" : this.read="not read"
+    read ? this.read ="read" : this.read="Not read"
 }
-
-function Info(){
-    
-}
-
-Info.prototype.bookInfo = function(){
-    return this.title +", "+this.author +", " + this.pages +", " + this.read;
-}
-
-Book.prototype = Object.create(Info.prototype);
 
 function addBookToLibrary(book){
     myLibrary.push(book);
@@ -28,13 +18,30 @@ function addBookToLibrary(book){
 function render(myLibrary){
     for(let book of myLibrary){
     let bookCard = document.createElement("container");
-    bookCard.innerText = book.bookInfo();
+    let title = document.createElement("h6")
+    title.innerText = "Title: " + book.title;
+    let author = document.createElement("h6")
+    author.innerText = "By: " + book.author;
+    let pages = document.createElement("h6")
+    pages.innerText = "Pages: " + book.pages;
+    let read = document.createElement("h6")
+    read.innerText = "status: " + book.read;
+    let deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    let statusButton = document.createElement("button");
+    statusButton.innerText = "Change Status";
+    bookCard.append(title);
+    bookCard.append(author);
+    bookCard.append(pages);
+    bookCard.append(read);
+    bookCard.append(deleteButton);
+    bookCard.append(statusButton);
     main.append(bookCard);
     }
 }
 
 
-const book1 = new Book("KOBE", "Bryant", 24, true)
+const book1 = new Book("The Hobbit", "Bryant", 24, false)
 
 addBookToLibrary(book1);
 render(myLibrary)
