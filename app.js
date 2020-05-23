7e0e6066474705bebe7561263b8984e5379d7e4a
Jackson Cai <jcai8649@gmail.com>
2020-05-22 19:35:35 -0700

    Add form submission

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ let myLibrary = [];
 
 
 function Book(title, author, pages, read){
+    debugger
     this.title = title;
     this.author = author;
     this.pages = pages;
-    read ? this.read ="read" : this.read="Not read"
+    this.read = read;
 }
 
 function addBookToLibrary(book){
@@ -17,7 +18,9 @@ function addBookToLibrary(book){
 
 function render(myLibrary){
     for(let book of myLibrary){
-    let bookCard = document.createElement("container");
+    let bookCard = document.createElement("section");
+    bookCard.classList.add("contain")
+    bookCard.classList.add("text-center");
     let title = document.createElement("h6")
     title.innerText = "Title: " + book.title;
     let author = document.createElement("h6")
@@ -27,8 +30,10 @@ function render(myLibrary){
     let read = document.createElement("h6")
     read.innerText = "status: " + book.read;
     let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
     deleteButton.innerText = "Delete";
     let statusButton = document.createElement("button");
+    statusButton.classList.add("changeStat");
     statusButton.innerText = "Change Status";
     bookCard.append(title);
     bookCard.append(author);
@@ -40,8 +45,60 @@ function render(myLibrary){
     }
 }
 
+function addBook(){
+    debugger;
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let status = document.querySelectorAll(`input[name="status"]`);
+    let selectedValue;
+    for (let stat of status){
+        if(stat.checked){
+            selectedValue = stat.value;
+            break;
+        } 
+    }
+    if (validation()){
+        formReset();
+        let newTitle = new Book(title, author, pages, selectedValue);
+        addBookToLibrary(newTitle);
+        render(myLibrary);
+    } else {
+        alert("Please fill in all the information")
 
-const book1 = new Book("The Hobbit", "Bryant", 24, false)
+    }
+}
 
-addBookToLibrary(book1);
-render(myLibrary)
+function changeRead(){
+
+}
+
+function deleteBook(){
+
+}
+
+function formReset(){
+    document.querySelector("form").reset();
+}
+
+function validation(){
+    debugger;
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let status = document.querySelectorAll(`input[name="status"]`);
+    let selectedValue;
+    for (const stat of status){
+        if(stat.checked){
+            selectedValue = stat.value;
+            break;
+        }
+    }
+
+    if(title === "" || author === "" || pages === "" || !selectedValue){
+        return false;
+    }
+    return true;
+
+}
+
